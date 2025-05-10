bd026fa6743903ac879ef1a8e529586c8aaa3a6c
BD Inventory System <bd-inventory@example.com>
2025-05-09 19:39:52 -0600

    changed text

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <v-app>
         <v-app-bar>
-            <v-app-bar-title class="text-h2 font-weight-bold text-center text-secondary">BATCH TRACKER</v-app-bar-title>
+            <v-app-bar-title class="text-h2 font-weight-bold text-center text-secondary">CWS BATCH TRACKER</v-app-bar-title>
         </v-app-bar>
         <v-main>
             <v-container fluid class="pa-0 h-100">
-                <DepartureRow time="TIME" destination="BATCH" gate="CAM" status="STATUS" />
+                <DepartureRow time="TIME" destination="LOT NUM" gate="CAM" status="STATUS" />
                 <v-divider thickness="5" class="my-5"></v-divider>
 
 
